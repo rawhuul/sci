8d2b4c8d7fc14c989ec93c05c4f3bc3fd1be6711
Rahul <basicfunc@gmail.com>
2023-06-19 21:47:53 +0530

    Project start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use git2::{Repository, StatusOptions};
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("[ERROR]: Destination to repository must be passed.");
+        return;
+    }
+
+    match Repository::open(&args[1]) {
+        Ok(repo) => {
+            let mut options = StatusOptions::new();
+            options.include_untracked(true);
+            options.include_ignored(false);
+
+            if let Ok(statuses) = repo.statuses(Some(&mut options)) {
+                let has_changes = !statuses.is_empty();
+                println!("Repository has changes: {}", has_changes);
+
+                if has_changes {
+                    for entry in statuses.iter() {
+                        if entry.status().is_index_new() {
+                            if let Some(head) = repo.head().ok() {
+                                if let Ok(commit) = head.peel_to_commit() {
+                                    println!("Committed changes found:");
+                                    println!("Commit ID: {}", commit.id());
+                                }
+                            }
+                        }
+                    }
+                }
+            } else {
+                println!("Failed to retrieve repository status.");
+            }
+        }
+        Err(e) => {
+            println!("Failed to open repository: {}", e);
+        }
+    }
 }
