8a2ff75afe10c3b0fa37c8c1c7a4665c9103645c
Rahul <basicfunc@gmail.com>
2023-06-21 16:06:38 +0530

    Faster key generation and removed unstable spinner.

--- src/dispatcher.rs
+++ src/dispatcher.rs
@@ -1,24 +1,19 @@
-use md5::Digest;
-use std::{net::SocketAddr, time::Duration};
+use std::net::SocketAddr;
 use tokio::{
     io::AsyncBufReadExt,
     net::{TcpListener, TcpStream},
 };
 
 use crate::watcher::Changes;
-use indicatif::{ProgressBar, ProgressStyle};
 
 pub struct Dispatcher {
     addr: SocketAddr,
-    msg_key: String,
+    key: u128,
 }
 
 impl Dispatcher {
-    pub fn new(addr: SocketAddr, msg_key: Digest) -> Self {
-        Dispatcher {
-            addr,
-            msg_key: format!("{msg_key:?}"),
-        }
+    pub fn new(addr: SocketAddr, key: u128) -> Self {
+        Dispatcher { addr, key }
     }
 
     pub async fn start(&self) -> Result<(), Box<dyn std::error::Error>> {
@@ -26,36 +21,20 @@ impl Dispatcher {
 
         println!("Dispatcher listening on {}", self.addr);
 
-        let spinner = ProgressBar::new_spinner();
-        spinner.set_style(
-            ProgressStyle::default_spinner()
-                .tick_chars("⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏")
-                .template("{spinner} {msg}")
-                .unwrap(),
-        );
-        spinner.set_message("Waiting for incoming connections");
-        spinner.enable_steady_tick(Duration::from_millis(100));
-
         while let Ok((stream, _)) = listener.accept().await {
-            let k = self.msg_key.clone();
-            let spinner_clone = spinner.clone();
+            let k = self.key;
 
             tokio::spawn(async move {
-                if let Err(err) = Self::handle_client(stream, &k).await {
+                if let Err(err) = Self::handle_client(stream, k).await {
                     eprintln!("Error handling client: {}", err);
                 }
-                spinner_clone.set_message("Waiting for incoming connections");
             });
         }
 
-        spinner.finish_and_clear();
         Ok(())
     }
 
-    async fn handle_client(
-        stream: TcpStream,
-        msg_key: &str,
-    ) -> Result<(), Box<dyn std::error::Error>> {
+    async fn handle_client(stream: TcpStream, key: u128) -> Result<(), Box<dyn std::error::Error>> {
         let mut reader = tokio::io::BufReader::new(stream);
         let mut line = String::new();
 
@@ -65,7 +44,7 @@ impl Dispatcher {
                 break;
             }
 
-            Self::handle_message(line.trim(), msg_key);
+            Self::handle_message(line.trim(), key);
 
             line.clear();
         }
@@ -73,9 +52,9 @@ impl Dispatcher {
         Ok(())
     }
 
-    fn handle_message(message: &str, msg_key: &str) {
-        if let Some((key, changes)) = message.split_once(":") {
-            if key == msg_key {
+    fn handle_message(message: &str, key: u128) {
+        if let Some((k, changes)) = message.split_once(":") {
+            if k.parse::<u128>().unwrap() == key {
                 if let Ok(changes) = serde_json::from_str::<Changes>(&changes) {
                     // Perform your desired action based on the received changes
                     println!("Received: {:?}", changes);
--- src/utils.rs
+++ src/utils.rs
@@ -1,19 +1,19 @@
-use md5::{compute, Digest};
-use rand::Rng;
+use rand::random;
 use std::net::SocketAddr;
+use std::time::{SystemTime, UNIX_EPOCH};
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpStream;
 
 pub struct Utils {
     addr: SocketAddr,
-    key: Digest,
+    key: u128,
 }
 
 impl Utils {
     pub fn new(addr: SocketAddr) -> Self {
         Self {
             addr,
-            key: generate_header_key(100000, 1000000),
+            key: generate_key(),
         }
     }
 
@@ -25,15 +25,56 @@ impl Utils {
         Ok(())
     }
 
-    pub fn key(&self) -> Digest {
+    pub fn key(&self) -> u128 {
         self.key
     }
 }
 
-pub fn generate_header_key(min: usize, max: usize) -> Digest {
-    let mut rng = rand::thread_rng();
-    let random = rng.gen_range(min..max);
-    let key = random ^ 0x17012001;
+fn generate_key() -> u128 {
+    // Generate a random number
+    let random_number: u128 = random();
 
-    compute(format!("{key}"))
+    // Get the current UNIX timestamp
+    let timestamp = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Failed to retrieve UNIX timestamp")
+        .as_secs();
+
+    // Find a large prime number near the timestamp
+    let prime_number = find_prime_near_timestamp(timestamp);
+
+    // Perform binary operations with 0x17012001
+    (prime_number ^ random_number) & 0x17012001
+}
+
+fn find_prime_near_timestamp(timestamp: u64) -> u128 {
+    // Start searching for a prime number near the timestamp
+    let mut candidate = timestamp as u128;
+    while !is_prime(candidate) {
+        candidate += 1;
+    }
+
+    candidate
+}
+
+fn is_prime(n: u128) -> bool {
+    if n <= 1 {
+        return false;
+    }
+    if n <= 3 {
+        return true;
+    }
+    if n % 2 == 0 || n % 3 == 0 {
+        return false;
+    }
+
+    let mut i = 5;
+    while i * i <= n {
+        if n % i == 0 || n % (i + 2) == 0 {
+            return false;
+        }
+        i += 6;
+    }
+
+    true
 }
