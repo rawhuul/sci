d9b7b6d8238e5b780f1c173c5e9d67fd9a41996b
Rahul <basicfunc@gmail.com>
2023-06-21 16:21:21 +0530

    More Strong Key Generator using LFSR

--- src/utils.rs
+++ src/utils.rs
@@ -31,50 +31,49 @@ impl Utils {
 }
 
 fn generate_key() -> u128 {
-    // Generate a random number
-    let random_number: u128 = random();
+    const SEED: u32 = 0x17012001 ^ 0xDEADBEEF;
+    const LFSR_MASK: u32 = 0xB4000001;
+    const LFSR_TAPS: u32 = 0x8020001;
+    const LFSR_ROUNDS: u32 = 32;
 
-    // Get the current UNIX timestamp
-    let timestamp = SystemTime::now()
+    let current_time = SystemTime::now()
         .duration_since(UNIX_EPOCH)
-        .expect("Failed to retrieve UNIX timestamp")
-        .as_secs();
+        .expect("System time went backwards")
+        .as_millis();
 
-    // Find a large prime number near the timestamp
-    let prime_number = find_prime_near_timestamp(timestamp);
+    let random_number: u32 = random();
 
-    // Perform binary operations with 0x17012001
-    (prime_number ^ random_number) & 0x17012001
-}
+    let get_prime_number = |time: u128, random: u32| -> u32 {
+        let mut number = (time as u32) ^ random;
+        number = number.wrapping_mul(SEED);
 
-fn find_prime_near_timestamp(timestamp: u64) -> u128 {
-    // Start searching for a prime number near the timestamp
-    let mut candidate = timestamp as u128;
-    while !is_prime(candidate) {
-        candidate += 1;
-    }
-
-    candidate
-}
-
-fn is_prime(n: u128) -> bool {
-    if n <= 1 {
-        return false;
-    }
-    if n <= 3 {
-        return true;
-    }
-    if n % 2 == 0 || n % 3 == 0 {
-        return false;
-    }
-
-    let mut i = 5;
-    while i * i <= n {
-        if n % i == 0 || n % (i + 2) == 0 {
-            return false;
+        while !(2..number).any(|i| number % i == 0) {
+            number = number.wrapping_add(1);
         }
-        i += 6;
-    }
 
-    true
+        number
+    };
+
+    let lfsr = |seed: u32| -> u32 {
+        let mut state = seed;
+
+        for _ in 0..LFSR_ROUNDS {
+            let lsb = state & 1;
+            state >>= 1;
+            if lsb == 1 {
+                state ^= LFSR_TAPS;
+            }
+        }
+
+        state &= LFSR_MASK;
+
+        state
+    };
+
+    let prime_number = get_prime_number(current_time, random_number);
+    let lfsr_output = lfsr(random_number);
+
+    let result: u128 = (prime_number as u128) << 32 | (lfsr_output as u128);
+
+    result
 }
